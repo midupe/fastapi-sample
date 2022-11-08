267d6c0e6cea8377f081a458cddbb6a7843e24e1
midupe <miguelpereira2000@gmail.com>
2022-11-08 13:14:25 +0000

    up

--- src/config/main.py
+++ src/config/main.py
@@ -4,9 +4,12 @@ from config.db import create_db_and_tables
 
 app = FastAPI()
 
+"""
+#auto generate database tables
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
+"""
 
 from app.views import *
     
